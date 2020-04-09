c41070af2f2b566a6b88732d61b15a3cf96fbe13
AdeBC <ch37915405887@gmail.com>
2020-04-09 11:59:09 +0800

    preprocessing update

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 from functools import reduce
+from tqdm import tqdm, trange
 import re
 
 
@@ -9,7 +10,7 @@ class Preprocessor(object):
 	def __init__(self, input_file, output_file):
 		content = self.readlines_from_txt(input_file)
 
-		## replace host-associated with host_associated, testing needed
+		## replace host-associated with host_associated
 		self.content = self.fix_host_associated(content)
 		df = self.parse_from_lines(self.content)
 		df = self.mutate_nlayers_from_labels(df)
@@ -23,7 +24,8 @@ class Preprocessor(object):
 		:param content:
 		:return:
 		"""
-		return [i.replace('Host-associated', 'Host_associated') for i in content]
+		print('Fixing errors in unlayered data')
+		return [i.replace('Host-associated', 'Host_associated') for i in tqdm(content)]
 
 	def readlines_from_txt(self, file_dir):
 		"""
@@ -31,9 +33,11 @@ class Preprocessor(object):
 		:param file_dir:
 		:return:
 		"""
+		print('Reading data......', end='')
 		with open(file_dir, 'r') as f:
 			lines = f.readlines()
 		lines = [line.rstrip('\n') for line in lines]
+		print('finished !')
 		return lines
 
 	def parse_from_lines(self, lines):
@@ -42,9 +46,11 @@ class Preprocessor(object):
 		:param lines:
 		:return:
 		"""
+		print('Parsing text data to dataframe......', end='')
 		parts_tmp = [line.split(' ') for line in lines]
 		df_values = [line[0].split('|') + [line[1].split('|')[1]] + [line[2]] for line in parts_tmp]
 		data = pd.DataFrame(df_values, columns=['sample_id', 'pred_labels', 'true_label', 'probas'])
+		print('finished !')
 		return data
 
 	def mutate_nlayers_from_labels(self, df):
@@ -53,9 +59,11 @@ class Preprocessor(object):
 		:param df:
 		:return:
 		"""
+		print('Calculating the number of layers for each label......', end='')
 		pred_labels = df['pred_labels']
 		plabels_split = pred_labels.apply(lambda x: x.split(','))
 		df['nlayers'] = plabels_split.apply(lambda x: [count_pattern('-', i) + 1 for i in x])
+		print('finished !')
 		return df
 
 	def extract_layers(self, df):
@@ -64,12 +72,13 @@ class Preprocessor(object):
 		:param df:
 		:return:
 		"""
+		print('Recalculating the label for each layer')
 		pred_labels = df['pred_labels'].apply(lambda x: x.split(','))
 		df['pred_labels'] = pred_labels.apply(lambda x: ','.join([','.join(extract_path(i)) for i in x]))
 		df['true_label'] = df['true_label'].apply(lambda x: ','.join(extract_path(x)))
 		nlayers = df['nlayers']
 		probas = df['probas'].apply(lambda x: ['%.3f' % float(i) for i in x.split(',')])
-		df['probas'] = [','.join(map(str, rep_elements(probas[i], nlayers[i]))) for i in range(len(probas))]
+		df['probas'] = [','.join(map(str, rep_elements(probas[i], nlayers[i]))) for i in trange(len(probas))]
 		return df
 
 	def to_text(self, df, output_file):
@@ -81,6 +90,7 @@ class Preprocessor(object):
 
 		with open(output_file, 'w') as f:
 			f.write('\n'.join(df['text']))
+		print('Results are saved in {}'.format(output_file))
 
 
 def rep_elements(elements, times):
